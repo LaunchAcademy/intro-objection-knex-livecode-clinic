122ec4b55aae9945f51f0bef3f459ee13dd8f178
Stephen Fang <safang99@gmail.com>
2021-01-10 21:23:28 -0500

    update script and boardgame to camelCase

--- server/src/routes/clientRouter.js
+++ server/src/routes/clientRouter.js
@@ -1,26 +1,26 @@
-import express from "express";
-import { fileURLToPath } from "url";
-import path, { dirname } from "path";
-import clientMiddlewares from "../middlewares/clientMiddlewares.js";
-import config from "../config.js";
+import express from "express"
+import { fileURLToPath } from "url"
+import path, { dirname } from "path"
+import clientMiddlewares from "../middlewares/clientMiddlewares.js"
+import config from "../config.js"
 
-const router = new express.Router();
-const currentPath = dirname(fileURLToPath(import.meta.url));
+const router = new express.Router()
+const currentPath = dirname(fileURLToPath(import.meta.url))
 
-let indexPath = path.join(currentPath, "../../public/dist/index.html");
+let indexPath = path.join(currentPath, "../../public/dist/index.html")
 if (config.nodeEnv !== "production") {
-  indexPath = path.join(currentPath, "../../../client/public/index.html");
+  indexPath = path.join(currentPath, "../../../client/public/index.html")
 }
 
-const clientRoutes = ["/", "/client", "/name"];
+const clientRoutes = ["/", "/client", "/name"]
 
 router.get(clientRoutes, (req, res) => {
-  res.sendFile(indexPath);
-});
+  res.sendFile(indexPath)
+})
 
-const middlewares = await clientMiddlewares();
+const middlewares = await clientMiddlewares()
 if (middlewares.length > 0) {
-  router.use(middlewares);
+  router.use(middlewares)
 }
 
-export default router;
+export default router
--- server/src/routes/rootRouter.js
+++ server/src/routes/rootRouter.js
@@ -1,7 +1,8 @@
-import express from "express";
-import clientRouter from "./clientRouter.js";
-const rootRouter = new express.Router(); //place your server-side routes here
+import express from "express"
+import clientRouter from "./clientRouter.js"
 
-rootRouter.use("/", clientRouter);
+const rootRouter = new express.Router() //place your server-side routes here
 
-export default rootRouter;
+rootRouter.use("/", clientRouter)
+
+export default rootRouter
